ff88193281c41879974b89970e1d3e6a8f774c6c
nasby26 <nasby26@gmail.com>
2022-12-15 20:57:49 +0900

    폴더구조 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// 모듈
+const express = require('express');
+const app = express();
+
+// 라우팅
+const home = require('./src/routes/home');
+
+// 앱 세팅
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+// use -> middleware를 등록하는 메서드
+app.use("/", home); 
+
+module.exports = app;
