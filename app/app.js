7fb844922afa94191e3da15cf3a02ad069e0a264
nasby26 <nasby26@gmail.com>
2022-12-15 21:13:11 +0900

    front-side javascript 생성

--- app/app.js
+++ app/app.js
@@ -10,7 +10,9 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
 // use -> middleware를 등록하는 메서드
-app.use("/", home); 
+app.use(express.static(`${__dirname}/src/public`)); // 정적경로 생성 관련 
+app.use("/", home);
 
 module.exports = app;
