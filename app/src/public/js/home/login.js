05c58b93663d1c888413c5d4239c006460e4cc51
nasby26 <nasby26@gmail.com>
2022-12-16 20:01:06 +0900

    DOM을 이용한 HTML객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
+// 프론트 단의 로그인 기능: 아이디와 비밀번호 값을 입력 받고 submit이 될 때에 그 정보를 서버로 넘겨주는 것
+
 'use strict';
 
-console.log('hello');
-console.log('bye');
+const id = document.getElementById('id');
+const pw = document.getElementById('pw');
+const loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+}
